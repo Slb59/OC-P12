6bd8f5916d497f9da04b77e5e142298fce9574c5
Slb59 <sylvie.bricout2@laposte.net>
2023-10-07 10:41:00 +0200

    add test stories 14_16

--- tests/functionnal/test_stories_14_16.py
+++ tests/functionnal/test_stories_14_16.py
@@ -8,9 +8,6 @@ def test_story_14(runner, epicstories):
 
     epicstories.setattr(
         EmployeeView,
-        'prompt_role', MockFunction.mock_role_manager)
-    epicstories.setattr(
-        EmployeeView,
         'prompt_employee', MockFunction.mock_employee_support)
 
     result = runner.invoke(epicevent.main, ['employee', 'inactivate'])
@@ -19,79 +16,69 @@ def test_story_14(runner, epicstories):
 
     assert not result.exception
     assert "Vos modifications ont été enregistrées" in result.output
-    assert "Aritomo     │       │ Manager" in result2.output
+    assert "<<Aritomo>> │       │ Support    │ Inactif" in result2.output
     assert "Affecter un support pour l'évènement Aritomo" in result3.output
 
 
-# def test_story_12_with_clients(runner, epicstories):
+def test_story_15_with_clients(runner, epicstories):
 
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_role', MockFunction.mock_role_manager)
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_employee', MockFunction.mock_yuka)
+    epicstories.setattr(
+        EmployeeView,
+        'prompt_employee', MockFunction.mock_yuka)
 
-#     result = runner.invoke(epicevent.main, ['employee', 'update-role'])
-#     result2 = runner.invoke(epicevent.main, ['employee', 'list'])
+    result = runner.invoke(epicevent.main, ['employee', 'inactivate'])
+    result2 = runner.invoke(epicevent.main, ['employee', 'list'])
 
-#     assert not result.exception
-#     assert "Ce commercial gère des contracts actifs" in result.output
-#     assert "Yuka        │       │ Commercial" in result2.output
+    assert not result.exception
+    assert "Ce commercial gère des contracts actifs" in result.output
+    assert "Yuka        │       │ Commercial" in result2.output
 
 
-# def test_story_12_without_clients(runner, epicstories):
+def test_story_15_without_clients(runner, epicstories):
 
-#     epicstories.setattr(EmployeeView,
-#                         'prompt_role', MockFunction.mock_role_manager)
-#     epicstories.setattr(EmployeeView,
-#                         'prompt_employee', MockFunction.mock_morihei)
+    epicstories.setattr(EmployeeView,
+                        'prompt_employee', MockFunction.mock_morihei)
 
-#     result = runner.invoke(epicevent.main, ['employee', 'update-role'])
-#     result2 = runner.invoke(epicevent.main, ['employee', 'list'])
+    result = runner.invoke(epicevent.main, ['employee', 'inactivate'])
+    result2 = runner.invoke(epicevent.main, ['employee', 'list'])
 
-#     assert not result.exception
-#     assert "Vos modifications ont été enregistrées" in result.output
-#     assert "Morihei     │       │ Manager" in result2.output
+    assert not result.exception
+    assert "Vos modifications ont été enregistrées" in result.output
+    assert "<<Morihei>> │       │ Commercial │ Inactif" in result2.output
 
 
-# def test_story_13_fail(runner, epicstories):
+def test_story_16_fail(runner, epicstories):
 
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_role', MockFunction.mock_role_commercial)
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_employee', MockFunction.mock_osynia)
+    epicstories.setattr(
+        EmployeeView,
+        'prompt_employee', MockFunction.mock_osynia)
 
-#     result = runner.invoke(epicevent.main, ['employee', 'update-role'])
-#     result2 = runner.invoke(epicevent.main, ['employee', 'list'])
+    result = runner.invoke(epicevent.main, ['employee', 'inactivate'])
+    result2 = runner.invoke(epicevent.main, ['employee', 'list'])
 
-#     assert not result.exception
-#     assert "La base doit contenir au moins un manager" in result.output
-#     assert "Osynia      │       │ Manager" in result2.output
+    assert not result.exception
+    assert "La base doit contenir au moins un manager" in result.output
+    assert "Osynia      │       │ Manager" in result2.output
 
 
-# def test_story_13(runner, epicstories):
+def test_story_16(runner, epicstories):
 
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_role', MockFunction.mock_role_manager)
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_employee', MockFunction.mock_morihei)
-#     result = runner.invoke(epicevent.main, ['employee', 'update-role'])
+    epicstories.setattr(
+        EmployeeView,
+        'prompt_role', MockFunction.mock_role_manager)
 
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_role', MockFunction.mock_role_commercial)
-#     epicstories.setattr(
-#         EmployeeView,
-#         'prompt_employee', MockFunction.mock_osynia)
+    epicstories.setattr(
+        EmployeeView,
+        'prompt_employee', MockFunction.mock_morihei)
+    result = runner.invoke(epicevent.main, ['employee', 'update-role'])
 
-#     result = runner.invoke(epicevent.main, ['employee', 'update-role'])
-#     result2 = runner.invoke(epicevent.main, ['employee', 'list'])
+    epicstories.setattr(
+        EmployeeView,
+        'prompt_employee', MockFunction.mock_morihei)
 
-#     assert not result.exception
-#     assert "Vos modifications ont été enregistrées" in result.output
-#     assert "ERROR : Accès refusé, rôle manager requis." in result2.output
+    result = runner.invoke(epicevent.main, ['employee', 'inactivate'])
+    result2 = runner.invoke(epicevent.main, ['employee', 'list'])
+
+    assert not result.exception
+    assert "Vos modifications ont été enregistrées" in result.output
+    assert "<<Morihei>> │       │ Manager    │ Inactif" in result2.output
